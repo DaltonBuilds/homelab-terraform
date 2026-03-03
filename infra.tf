dd47b09217cf027f5cfafc6157f2bafa65a75a7c
Dalton <daltonjousley@gmail.com>
2026-03-03 13:39:30 -0700

    feat(proxmox): add infrastructure and local configuration for VM setup, including Ubuntu image download and cloud-init snippets

---
+++ infra.tf
@@ -0,0 +1,70 @@
+# Download once, reused by each VM
+resource "proxmox_virtual_environment_download_file" "ubuntu_image" {
+    node_name = var.node_name
+    content_type = "import"
+    datastore_id = "local"
+    url = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+    file_name = "noble-server-cloudimg-amd64.qcow2"
+}
+
+# Upload per-VM cloud-init snippets
+resource "proxmox_virtual_environment_file" "cloud_init" {
+  for_each = local.nodes
+  node_name = var.node_name
+  content_type   = "snippets"
+  datastore_id   = "local"
+
+  source_raw {
+    data = each.value.snippet
+    file_name = "${each.key}.yaml"
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "nodes" {
+    for_each = local.nodes
+    name      = each.key
+    vm_id     = each.value.id
+    node_name = var.node_name
+    tags      = each.value.tags
+
+    agent {
+        enabled = true
+    }
+    
+    stop_on_destroy = true
+
+    cpu {
+        cores = each.value.cores
+        type = "x86-64-v2-AES"
+    }
+
+    memory {
+        dedicated = each.value.memory
+    }
+
+    disk {
+        datastore_id = "local-lvm"
+        interface = "scsi0"
+        size = 20
+        import_from = proxmox_virtual_environment_download_file.ubuntu_image.id
+    }
+
+    network_device {
+        bridge = "vmbr0"
+    }
+
+    operating_system {
+        type = "l26"
+    }
+
+    initialization {
+        user_data_file_id = proxmox_virtual_environment_file.cloud_init[each.key].id
+
+        ip_config {
+            ipv4 {
+                address = "${each.value.ip}/24"
+                gateway = var.gateway_ip
+            }
+        }
+    }
+}
---
+++ locals.tf
@@ -0,0 +1,28 @@
+locals {
+    nodes = {
+        "gimli" = {
+            id      = 101
+            ip      = var.ip_addresses["gimli"]
+            snippet = file("${path.module}/cloud-init/gimli.yaml")
+            cores   = 4
+            memory  = 4096
+            tags    = ["node", "homelab", "k3s-worker", "cluster-workloads"]
+        }
+        "nfs-server" = {
+            id      = 102
+            ip      = var.ip_addresses["nfs-server"]
+            snippet = file("${path.module}/cloud-init/nfs-server.yaml")
+            cores   = 2
+            memory  = 2048
+            tags    = ["node", "homelab", "storage"]
+        }
+        "mgmt-plane" = {
+            id      = 103
+            ip      = var.ip_addresses["mgmt-plane"]
+            snippet = file("${path.module}/cloud-init/mgmt-plane.yaml")
+            cores   = 2
+            memory  = 2048
+            tags    = ["node", "homelab", "cluster-management"]
+        }
+    }
+}
